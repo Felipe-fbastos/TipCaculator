ede1bed92fd4052d628e8d459d8de3e898c81105
Felipe-fbastos <felipeferreirabastos2@gmail.com>
2024-08-23 20:52:42 -0300

    Criação Interface

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void setLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
